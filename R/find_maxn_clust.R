3b24009ff7ac8e3c130d7e7b6259e15804b4cd68
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2022-01-12 12:23:25 -0600

    fixing errors in arguments

--- R/find_maxn_clust.R
+++ R/find_maxn_clust.R
@@ -2,14 +2,13 @@
 #'
 #' @param x A data.frame or matrix object containing eigenvectors by sites data.
 #' @param threshold Scalar. The number of eigenvectors used to perform classification.
-#' @param runs Scalar. Number of times classification will be performed.
+#' @param nperm Scalar. Number of times classification will be performed.
 #' @param method c("kmeans","ward"). See \code{\link{find.clusters}} of adegenet package.
 #' @param stat c("BIC", "AIC", or "WSS"). See \code{\link{find.clusters}} of adegenet package.
 #' @param criterion c("diffNgroup", "min","goesup", "smoothNgoesup", or "goodfit"). See \code{\link{find.clusters}} of adegenet package.
 #' @param max.nclust Value of set of values defining the maximum number of groups to be evaluated.
 #' @param subset number of cells used in the analysis. It is particularly important whenever the total number of cells is large (> 1000).
 #' @param confidence.level threshold confidence level used to estimate congruence in the classification pattern.
-#' @param esp Coordinates for assemblages
 #'
 #' @return Matrix containing congruence values ranging between 0-1 for each max.nclust value (see Arguments) and confidence level.
 #' 
@@ -18,8 +17,7 @@
 #' @examples
 find.max.nclust <- function(x, 
                             threshold, 
-                            esp,
-                            runs = 100, 
+                            nperm = 100, 
                             method = "kmeans", 
                             stat = "BIC", 
                             criterion = "diffNgroup", 
@@ -28,7 +26,7 @@ find.max.nclust <- function(x,
                             ) 
   {
   group.affinity <- matrix(NA, subset, subset)
-  grouping <- matrix(NA, (subset*(subset-1))/2, runs)
+  grouping <- matrix(NA, (subset*(subset-1))/2, nperm)
   group.sample <- sample(1:nrow(x), size = subset, replace = FALSE)
   congruence <- matrix(NA, length(max.nclust), length(confidence.level), dimnames = list(paste("max.", 
                                                                                                max.nclust, 
@@ -37,7 +35,7 @@ find.max.nclust <- function(x,
                                                                                          paste("Confidence.level=", confidence.level, "%",sep="")))
   for (m in 1:length(confidence.level)){
     for (l in 1:length(max.nclust)){
-      for (k in 1:runs){
+      for (k in 1:nperm){
         clust.vec <- adegenet::find.clusters(x[, 1:threshold],
                                            clust = NULL,
                                            choose.n.clust = FALSE,
@@ -48,7 +46,7 @@ find.max.nclust <- function(x,
                                            criterion = criterion,
                                            max.n.clust = max.nclust[[l]]
         )
-        rownames(as.data.frame(clust.vec$grp)) == rownames(esp)
+        rownames(as.data.frame(clust.vec$grp)) == rownames(x)
         groups <- as.matrix(clust.vec$grp)
         group.subset <- as.matrix(groups[group.sample,])
         tgroups<-as.matrix(t(group.subset))
@@ -61,7 +59,7 @@ find.max.nclust <- function(x,
       }
       cor.grouping <- as.matrix(cor(grouping))
       diag(cor.grouping) <- NA
-      congruence[l, m] <- sum(ifelse(rowMeans(cor.grouping, na.rm = T) > confidence.level[[m]], 1, 0))/runs
+      congruence[l, m] <- sum(ifelse(rowMeans(cor.grouping, na.rm = T) > confidence.level[[m]], 1, 0))/nperm
     }
   }  
   congruence
