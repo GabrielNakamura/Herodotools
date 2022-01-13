97470cf82b73ea078b524a88192285f41e9f8493
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2022-01-13 17:07:45 -0600

    fixing issues in affiliation function

--- R/affiliation_evoreg.R
+++ R/affiliation_evoreg.R
@@ -1,29 +1,32 @@
 #' Affiliation values for assemblages according to phylogenetic turnover 
 #'
-#' @param evo.vectors An object returned from evoregions function
+#' @param evo.vectors An object returned from \code{\link{evoregions}} function
 #' @param n.groups Scalar indicating the number of groups that will be evaluated for affiliation
 #' @param method Character indicating the method used to compute the distance in fuzzy matrix
 #'
-#' @return A list with two elements containing affiliation values and species coordinates
+#' @return A list with two matrix, one containing affiliation values and the group in which each cell 
+#'     is classified and the other containing cell coordinates
+#'
 #' @export
 #'
 #' @examples
-afilliation.evoreg <- function(evo.vectors, n.groups = 4, method = "euclidean"){
+afilliation.evoreg <- function(evo.vectors,
+                               method = "euclidean"){
+  n.groups <- length(as.numeric(levels(evo.vectors[[2]]$grp)))
   groups.vec.bray <- evo.vectors[[2]]$grp
   vec.bray <- evo.vectors[[1]]
   Gs <- lapply(1:n.groups, function(x){
     which(groups.vec.bray == x)
   })
   names(Gs) <- paste("G", 1:n.groups, sep = "")
-  
   dist.P.fuzzy <- as.matrix(vegan::vegdist(
-                            x = vec.bray,
-                            method = "euclidean",
-                            diag = T,
-                            upper = T
-                            )
-                            )
-    
+    x = vec.bray,
+    method = method,
+    diag = T,
+    upper = T
+  )
+  )
+  
   PGall <- lapply(Gs, function(x){
     dist.P.fuzzy[x, x]
   })
@@ -38,7 +41,7 @@ afilliation.evoreg <- function(evo.vectors, n.groups = 4, method = "euclidean"){
   list_fuzzy.OGU.all.pad <- vector(mode = "list", length = length(fuzzy.OGU.x))
   for(i in 1:length(fuzzy.OGU.x)){
     fuzzy.OGU.x.pad <- scales::rescale(fuzzy.OGU.x[[i]], c(0, 1))
-    fuzzy.OGU.x.pad[[i]][, 2] <- i
+    fuzzy.OGU.x.pad[, 2] <- i
     list_fuzzy.OGU.all.pad[[i]] <- fuzzy.OGU.x.pad
   }
   fuzzy.OGU.all.pad <- do.call(rbind, list_fuzzy.OGU.all.pad)
@@ -47,8 +50,8 @@ afilliation.evoreg <- function(evo.vectors, n.groups = 4, method = "euclidean"){
   fuzzy.OGU.all.pad.org <- org$community
   fuzzy.OGU.all.pad.org[, 1] <- 1 - fuzzy.OGU.all.pad.org[, 1]
   esp.fuzzy <- org$environmental
-  rownames(fuzzy.OGU.all.pad.org) == rownames(esp.fuzzy)
   list_res <- vector(mode = "list", length = 2)
   list_res[[1]] <- esp.fuzzy
   list_res[[2]] <- fuzzy.OGU.all.pad.org
+  list_res
 }
