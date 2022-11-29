48424ce90e929637c059b7b89da4e70cc83ea348
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2022-11-29 11:20:22 -0300

    adding new auxilary function to compute spp per evoregion

---
+++ R/function_region_occ.R
@@ -0,0 +1,71 @@
+#' Species occurrence in each evoregion
+#'
+#' Auxiliary function to produce an occurrence data frame of species in each region. This object can be 
+#'     with \code{\link{tipranges_to_BioGeoBEARS}} function to produce a phyllip file needed in BioGeoBEARS
+#'     ancestral area reconstruction
+#'
+#' @param comm Occurrence data frame with species in colums and rows corresponding to assemblages
+#' @param site.region a character vector indicating the membership of each assemblage to regions
+#'
+#' @return An occurrence data frame with species as rownames and regions as columns
+#' @export
+#' 
+#' @seealso \code{\link{tipranges_to_BioGeoBEARS}}
+#'
+#' @author Gabriel Nakamura and Arthur Rodrigues
+#'
+#' @examples
+#' data(akodon.pa.tree)
+#' data(regions)
+#' site_evoregion <- regions$Cluster_Evoregions
+#' region_occ(comm=akodon.pa.tree,site.region=site_evoregion)
+region_occ <- function(comm, site.region){
+  evoregion.data <- 
+    # bind compostion matrix and the site evoregion
+    dplyr::bind_cols(
+      comm,
+      site.region =  site.region
+    ) %>% 
+    # transform the matrix in a longer dataframe 
+    tidyr::pivot_longer(
+      cols = 1:30, 
+      names_to = "species",
+      values_to = "presence"
+    ) %>% 
+    # filter only the rows with the presence of a species
+    dplyr::filter(presence == 1) %>% 
+    # Count the number of sites in a region that a species is present
+    dplyr::group_by(species, site.region) %>% 
+    dplyr::summarise(n = n()) %>% 
+    dplyr::ungroup() %>% 
+    # Calculate the propotional area of a species in each region
+    dplyr::group_by(species) %>% 
+    dplyr::mutate(
+      species.total = sum(n), 
+      prec.occupation = n/species.total
+    ) %>% 
+    dplyr::ungroup() %>% 
+    # Filter for each species the evoregions with more than 25% of area ocupied 
+    # by the species
+    dplyr::filter(
+      prec.occupation >= .25
+    ) %>% 
+    dplyr::mutate(
+      area = LETTERS[site.region]
+    ) %>% 
+    dplyr::select(species, area) %>% 
+    dplyr::mutate( value = 1) %>% 
+    # transform in a dataframe with species in rows and areas ocupied [0-1] in columns
+    tidyr::pivot_wider(
+      id_cols = species,
+      names_from = area, 
+      names_sort = T,
+      values_from = value,
+      values_fill = 0
+    ) %>% 
+    as.data.frame()
+  species.names <- evoregion.data[,1]
+  a.regions <- evoregion.data[,-1]
+  rownames(a.regions) <- species.names  
+  return(a.regions)
+}
