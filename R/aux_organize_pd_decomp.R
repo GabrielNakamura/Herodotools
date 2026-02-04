66f54b32dc912d0e7ce393c7401d2cf8e7921fe6
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2026-02-04 09:20:52 -0300

    fixing minor issues in pd_decomposition function

---
+++ R/aux_organize_pd_decomp.R
@@ -0,0 +1,20 @@
+#' Auxiliar to organize output from PD decomposition function
+#'
+#' @param list1.decomp data frame with PD components
+#' @param list2.treetable tibble with edge partition for each community
+#'
+#' @return A list object with decomposition of PD
+#' @export
+
+organize_pd_decomp <- 
+  function(list1.decomp, list2.treetable){
+    df_res <- do.call(rbind, list1.decomp)
+    list_res_final <- vector(mode = "list")
+    list_res_final$PD_decomposition <- df_res
+    list_res_final$tree_table_potential <- list2.treetable
+    
+    # matrix of PD decomposition - dense format
+    PD_decomposition_wide <- tidyr::pivot_wider(data = list_res_final$PD_decomposition, names_from = "partition", values_from = "value")
+    list_res_final$dense_matrix_PD <- PD_decomposition_wide
+    return(list_res_final)
+  }
